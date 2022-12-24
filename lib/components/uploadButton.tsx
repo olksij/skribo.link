98e7b56eee342bead73b886c098e4ac58ee625fe
Oleksii Besida <pixel.coder7@gmail.com>
2022-12-24 18:51:13 +0100

    loading state & copy

--- lib/components/uploadButton.tsx
+++ lib/components/uploadButton.tsx
@@ -1,6 +1,7 @@
 import styles from '../../styles/uploadButton.module.css'
 
 import { displayFont } from '../../pages/_app';
+import { textFont } from '../../pages/_app';
 
 // import firestore & utils
 import { database, storage } from '../../lib/firebase';
@@ -8,35 +9,47 @@ import { database, storage } from '../../lib/firebase';
 import { ref as databaseRef, set } from "firebase/database";
 import { ref as storageRef, uploadBytes } from "firebase/storage";
 
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Loading from './loading';
 
 export default function UploadButton() {
-  let [isLoading, setLoading] = useState<boolean>(false)
-  let [link, setLink] = useState<string>('')
+  let [state, setState] = useState<number>(0)
+  let [link, setLink] = useState<string | null>()
+  
+  let textRef = useRef<HTMLInputElement>(null)
+  let stateRef = useRef<HTMLParagraphElement>(null)
 
   const pickedFile = (e: any) => {
     let file = e.target.files[0] as File;
-    let id = (Math.random() * Math.pow(10, 16) + Date.now()).toString(26);
+    let id = Math.random().toString(36).substring(6) + Date.now().toString(36);
     let imageRef = storageRef(storage, `cards/${id}`);
 
-    setLoading(true);
+    setState(1);
 
     uploadBytes(imageRef, file).then((snapshot) => {
       const docRef = databaseRef(database, `cards/${id}`);
       set(docRef, { left: 24 });
-      setLink(`https://scratch.oleksii.xyz/${id}`);
-      setLoading(false)
+      textRef.current!.value = `https://scratch.oleksii.xyz/${id}`;
+      setState(2)
     });
   }
 
+  let buttonContent = state == 1 ? <Loading/> : <div>
+    <img src={state == 0 ? '/imageIcon.svg' : '/linkIcon.svg'}/>
+    <p ref={stateRef}>{state == 0 ? "Choose an image" : "Copy link"}</p>
+  </div>;
+
+  const click = () => {
+    textRef.current?.select();
+    document.execCommand("copy");
+    if (state == 2) stateRef.current!.innerText = 'Copied!';
+  }
+
   return <div className={styles.container + ' ' + displayFont.className}>
-    <label className={styles.label} htmlFor="file">
-      <div>
-        <img src="/imageIcon.svg"/>
-        <p>{status ? status : "Choose an image"}</p>
-      </div>
+    <label onClick={click} className={styles.label + ' ' + (state == 1 && styles.loading) + ' ' + (state == 2 && styles.copy)} htmlFor={state == 0 ? 'file' : ''}>
+      {buttonContent}
     </label>
+    <input className={styles.link + ' ' + textFont.className} tabIndex={-1} readOnly ref={textRef} id="input" type="text" />
     <input type="file" id="file" onChange={pickedFile} accept="image/*" style={{ opacity: '0', width: '0', height: '0' }}/>
   </div>
 }
