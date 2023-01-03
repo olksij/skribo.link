dc409a6a2f5d250ab07a008938b576f854822c5e
Oleksii Besida <besida@oleksii.xyz>
2023-01-03 12:25:19 +0100

    theme emoji animations

---
+++ lib/animations/import.ts
@@ -0,0 +1,7 @@
+import an0 from './0.json';
+import an1 from './1.json';
+import an2 from './2.json';
+import an3 from './3.json';
+
+const animations = [ an0, an1, an2, an3 ]
+export default animations;
--- lib/widgets/theme.tsx
+++ lib/widgets/theme.tsx
@@ -1,21 +1,36 @@
 
-import { CSSProperties, useState } from 'react'
+import { CSSProperties, useEffect, useRef, useState } from 'react'
 import backgrounds from '../backgrounds/import'
 import Card, { CardType } from '../elements/card'
 import Selectable from '../elements/selectable'
 import patterns from '../patterns/import'
 
-let emojis = ['🏠', '🍋', '🍓', '🐠']
+import { LottieOptions, useLottie } from "lottie-react";
+
+import animations from "../animations/import";
 
 export default function ThemesWidget({ id, theme, setTheme }: { id: number, theme: number, setTheme: any }) {
   const [scale, setScale] = useState<number>(1);
+  const [isPlaying, setPlaying] = useState<boolean>(false);
+
+  const options: LottieOptions = { animationData: animations[id], loop: false, autoplay: false, onComplete: () => setPlaying(false) };
+  const animation = useLottie(options), { View, animationItem } = animation;
+
+  useEffect(() => {
+    if (isPlaying) animation.play();
+    else animation.stop();
+  }, [animation, isPlaying]);
+
+  useEffect(() => {
+    if (theme == id) setPlaying(true)
+  }, [theme, id, scale]);
 
   return <Selectable id={id} selected={theme} setSelected={setTheme} borderRadius={6} scaleCallback={setScale}>
-      <div style={{ background: `url(${backgrounds[id]})`, ...styles.card }}>
-        <div style={{ background: `url(${patterns[id]})`, ...styles.pattern }}/>
-        <p style={{ ...styles.emoji, transform: `scale(${scale*scale})` }}>{emojis[id]}</p>
-      </div>
-    </Selectable>
+    <div style={{ background: `url(${backgrounds[id]})`, ...styles.card }}>
+      <div style={{ background: `url(${patterns[id]})`, ...styles.pattern }}/>
+      <p style={{ ...styles.emoji, transform: `scale(${scale*scale})`, height: isPlaying && id == theme ? '48px' : '32px' }}>{View}</p>
+    </div>
+  </Selectable>
 }
 
 let styles: Record<string, CSSProperties> = {
@@ -37,8 +52,8 @@ let styles: Record<string, CSSProperties> = {
   },
   emoji: { 
     placeSelf: 'center', 
-    fontSize: '24px', 
+    height: '48px', 
     textShadow: '0 2px 16px #0003',
-    transition: '.3s cubic-bezier(.5, 0, 0, 1)',
+    transition: '.5s cubic-bezier(.5, 0, 0, 1)',
   }
 }
