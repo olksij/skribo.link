1765bce7cbfe5b041c7fab9ee67c2eb784814be4
Oleksii Besida <besida@oleksii.xyz>
2023-01-05 20:12:03 +0100

    improved noise performance

--- lib/widgets/spoilerNoise.tsx
+++ lib/widgets/spoilerNoise.tsx
@@ -12,7 +12,9 @@ export default function SpoilerNoise() {
   let ratio  = useRef<number>();
 
   useEffect(() => {
-    ratio.current = devicePixelRatio * 4;
+    if (ratio.current != null) return;
+
+    ratio.current = devicePixelRatio;
     lastTime.current = performance.now();
 
     width .current = innerWidth  * ratio.current, canvasRef.current?.setAttribute('width',  width .current + 'px');
@@ -27,35 +29,52 @@ export default function SpoilerNoise() {
 
     context.clearRect(0, 0, width.current, height.current);
     const passed = time - (lastTime.current ?? time);
+    console.log(passed)
+
+    var now = performance.now();
     
     dotData.current = dotData.current.map(({ vx, vy, x, y, for: forN, since }: DotData, i) => {
-      //                                                           Total time assigned
-      //                                                           _________|__________
-      context!.fillStyle = '#FFF' + (15 - Math.round(Math.abs(forN - since) / (forN + since) * 15)).toString(16);
-      context!.fillRect(x, y, 10, 10);
-
-      vx /= 1.01, vy /= 1.01, x += vx * passed, y += vy * passed, forN -= passed, since += passed;
-
+      vx /= 1.25, vy /= 1.25, x += vx * passed, y += vy * passed, forN -= passed, since += passed;
       if (forN <= 0) { newDot(); return null }
-
       return { vx, vy, x, y, for: forN, since } as DotData;
-    }).filter(value => value != null 
-      && (value.x > 0 && value.y > 0 && value.x < width.current! && value.y < height.current!)) as DotData[];
+    }).filter(value => value != null && (value.x > 0 && value.y > 0 && value.x < width.current! && value.y < height.current!)) as DotData[];
+
+    //console.log(performance.now()-now)
+    var now = performance.now();
+
+    for (let multY = 0; (multY-1) * 800 < height.current; multY++ ) {
+      for (let multX = 0; (multX-1) * 800 < width.current; multX++ ) {
+        dotData.current.forEach(({ x, y, for: forN, since }) => { //   Total time assigned
+          //                                                           _________|__________
+          context!.fillStyle = '#FFF' + (15 - Math.round(Math.abs(forN - since) / (forN + since) * 15)).toString(16);
+          context!.fillRect(x + multX*800, y + multY*800, 3, 3);
+        })  
+      }
+    }
+
+    //console.log(performance.now()-now)
+    var now = performance.now();
 
     lastTime.current = time;
 
-    for (let i = 0; i < innerWidth * innerHeight / 100 - dotData.current.length; i++) newDot();
+    for (let i = 0; i < 200 * 200 / 100 - dotData.current.length; i++) newDot();
+
+    //console.log(performance.now()-now)
+    var now = performance.now();
 
     requestAnimationFrame(render);
   }
 
-  const newDot = () => dotData.current.push({ vx: random(0.5, true), vy: random(0.5, true), 
-    x: random(width.current), y: random(height.current), for: random(1000), since: 0 })
+  const newDot = () => dotData.current.push({ vx: random(.25, true, true), vy: random(.25, true, true), 
+    x: random(800), y: random(800), for: Math.abs(random(500)), since: 0 })
 
-  return <canvas style={{ position: 'absolute', width: '100%', height: '100%' }} ref={canvasRef}/>
+  return <canvas style={{ position: 'absolute', width: '100%', height: '100%', mixBlendMode: 'overlay' }} ref={canvasRef}/>
 };
 
-function random(max: number = 0, signed: boolean = false) {
-  let min = signed ? -max : 0;
-  return Math.random() * (max - min) + min;
+function random(max: number = 0, expo: boolean = false, signed: boolean = false) {
+  let random = Math.random() * 2 - 1;
+  let result = (expo ? dotPrb(dotPrb(random)) : Math.abs(random)) * (signed ? random / Math.abs(random) : 1) * max;
+  return result;
 }
+
+const dotPrb = (x: number) => (Math.cos(x * Math.PI + Math.PI) + 1) / 2;
