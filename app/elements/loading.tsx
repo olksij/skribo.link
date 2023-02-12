0bea6f27ab04a1646f3c584adcf688abe6c746aa
Oleksii Besida <besida@oleksii.xyz>
2023-02-12 22:39:34 +0100

    fixed loading indicator

--- app/elements/loading.tsx
+++ app/elements/loading.tsx
@@ -5,7 +5,11 @@ type LoadingProps = {
 }
 
 export default function Loading({ style }: LoadingProps) {
-  return <div style={{ ...styles.container, ...style }}><div style={ styles.child }/></div>
+  return <div style={{ ...styles.container, ...style }}>
+    <div style={{ ...styles.child, animationDelay: '0s' }}/>
+    <div style={{ ...styles.child, animationDelay: '.3s' }}/>
+    <div style={{ ...styles.child, animationDelay: '.6s' }}/>
+  </div>
 }
 
 let styles: Record<string, CSSProperties> = {
@@ -19,6 +23,9 @@ let styles: Record<string, CSSProperties> = {
     width: 8,
     content: '',
     background: '#3F3C49',
-    borderRadius: 4,  
+    borderRadius: 4,
+    animationName: 'loading',
+    animationDuration: '1.2s',
+    animationIterationCount: 'infinite',
   }
 }
